84aae888f93b947976c820af1681966b62cba7f5
xing-6464 <xing_6464@foxmail.com>
2023-03-07 20:36:20 +0800

    test alert

--- src/components/Alert/alert.test.tsx
+++ src/components/Alert/alert.test.tsx
@@ -6,8 +6,12 @@ describe('test alert component', () => {
     render(<Alert title="this is title"></Alert>)
 
     const element = screen.getByText(/this is title/i)
+    const element2 = screen.getByText(/关闭/i)
 
     expect(element.tagName).toEqual('SPAN')
-    expect(element).toHaveClass('star-alert-title bold-title')
+    expect(element).toHaveClass('star-alert-title')
+
+    expect(element2.tagName).toEqual('SPAN')
+    expect(element2).toHaveClass('star-alert-close')
   })
 })
