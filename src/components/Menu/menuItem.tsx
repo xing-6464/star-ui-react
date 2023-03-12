3163fa122a8ca89b2099b6b2034bc3c861040e13
xing-6464 <xing_6464@foxmail.com>
2023-03-12 14:21:07 +0800

    Menu 组件编码第三部分

--- src/components/Menu/menuItem.tsx
+++ src/components/Menu/menuItem.tsx
@@ -4,7 +4,7 @@ import classNames from 'classnames'
 import { MenuContext } from './menu'
 
 export interface MenuItemProps {
-  index: number
+  index?: number
   disabled?: boolean
   className?: string
   style?: React.CSSProperties
@@ -21,7 +21,7 @@ export const MenuItme: React.FC<MenuItemProps> = (props) => {
   })
 
   const handleClick = () => {
-    if (context.onSelect && !disabled) {
+    if (context.onSelect && !disabled && typeof index === 'number') {
       context.onSelect(index)
     }
   }
@@ -32,3 +32,5 @@ export const MenuItme: React.FC<MenuItemProps> = (props) => {
     </li>
   )
 }
+
+MenuItme.displayName = 'MenuItem'
