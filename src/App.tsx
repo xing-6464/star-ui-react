0d2d9a86bca9181ad7771622948b3f2b7ff4bf99
xing-6464 <xing_6464@foxmail.com>
2023-03-12 22:52:13 +0800

    添加图表库

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 import { Tabs, TabItem } from './components/Tabs'
 
 function App() {
   return (
     <div>
+      <FontAwesomeIcon icon={faCoffee} size="10x" />
       <Tabs defaultIndex={0} onSelect={() => {}} type="card">
         <TabItem label="card1">this is card one</TabItem>
         <TabItem label="card2">this is card one1</TabItem>
