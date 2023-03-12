92b180e6512bacf3472bfc622572e9d8540114a2
xing-6464 <xing_6464@foxmail.com>
2023-03-12 17:08:11 +0800

    SubMenu 组件添加测试

--- src/components/Menu/menu.test.tsx
+++ src/components/Menu/menu.test.tsx
@@ -1,26 +1,44 @@
 import React from 'react'
-import { fireEvent, render, screen } from '@testing-library/react'
-import { Menu, MenuItme } from '.'
+import { fireEvent, render, screen, waitFor } from '@testing-library/react'
+import { Menu, MenuItme, SubMenu } from '.'
 
 import type { MenuProps } from '.'
 
 const testProps: MenuProps = {
-  defaultIndex: 0,
+  defaultIndex: '0',
   onSelect: vi.fn(),
   className: 'test',
 }
 
 const testVerProps: MenuProps = {
-  defaultIndex: 0,
+  defaultIndex: '0',
   mode: 'vertical',
+  defaultOpenSubMenus: ['3'],
 }
 
+const createStyleFile = () => {
+  const cssFile: string = `
+    .star-submenu {
+      display: none;
+    }
+    .star-submenu.menu-opened {
+      display: block;
+    }
+  `
+  const style = document.createElement('style')
+  style.innerHTML = cssFile
+
+  return style
+}
 const generateMenu = (props: MenuProps) => {
   return (
     <Menu {...props}>
       <MenuItme>active</MenuItme>
       <MenuItme disabled>disabled</MenuItme>
       <MenuItme>xyz</MenuItme>
+      <SubMenu title="dropdown">
+        <MenuItme>drop1</MenuItme>
+      </SubMenu>
     </Menu>
   )
 }
@@ -29,7 +47,8 @@ let menuElement: HTMLElement | null,
   activeElement: HTMLElement | null,
   disabledElement: HTMLElement | null
 const setup = (props: MenuProps) => {
-  render(generateMenu(props))
+  const { container } = render(generateMenu(props))
+  container.append(createStyleFile())
   menuElement = screen.getByTestId('test-menu')
   activeElement = screen.getByText('active')
   disabledElement = screen.getByText('disabled')
@@ -46,7 +65,7 @@ describe('test Menu and MenuItem component', () => {
     setup(testProps)
     expect(menuElement).toBeInTheDocument()
     expect(menuElement).toHaveClass('star-menu test')
-    expect(menuElement?.getElementsByTagName('li').length).toEqual(3)
+    expect(menuElement?.querySelectorAll(':scope > li').length).toEqual(4)
     expect(activeElement).toHaveClass('menu-item is-active')
     expect(disabledElement).toHaveClass('menu-item is-disabled')
   })
@@ -58,16 +77,42 @@ describe('test Menu and MenuItem component', () => {
     fireEvent.click(thirdItem)
     expect(thirdItem).toHaveClass('is-active')
     expect(activeElement).not.toHaveClass('is-active')
-    expect(testProps.onSelect).toHaveBeenCalledWith(2)
+    expect(testProps.onSelect).toHaveBeenCalledWith('2')
 
     fireEvent.click(disabledElement!)
     expect(disabledElement).not.toHaveClass('is-active')
-    expect(testProps.onSelect).not.toHaveBeenCalledWith(1)
+    expect(testProps.onSelect).not.toHaveBeenCalledWith('1')
   })
 
   it('should render vertical mode when mode is set to vertical', () => {
     setup(testVerProps)
 
     expect(menuElement).toHaveClass('menu-vertical')
+  })
+
+  it('should show dropdown items when hover on subMenu', async () => {
+    setup(testProps)
+
+    expect(screen.queryByText('drop1')).not.toBeVisible()
+    const dropdownElement = screen.getByText('dropdown')
+
+    fireEvent.mouseEnter(dropdownElement)
+    await waitFor(() => {
+      expect(screen.queryByText('drop1')).toBeVisible()
+    })
+
+    fireEvent.click(screen.getByText('drop1'))
+    expect(testProps.onSelect).toHaveBeenCalledWith('3-0')
+
+    fireEvent.mouseLeave(dropdownElement)
+    await waitFor(() => {
+      expect(screen.queryByText('drop1')).not.toBeVisible()
+    })
+  })
+
+  it('vertical should show dropdown items when hover on subMenu', () => {
+    setup(testVerProps)
+
+    expect(screen.getByText('drop1')).toBeVisible()
   })
 })
