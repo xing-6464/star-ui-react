ac0b70363358b0b6710caf443ede6f4d0d3a197f
xing-6464 <xing_6464@foxmail.com>
2023-03-19 18:27:26 +0800

    transition组件

--- src/components/Transition/transition.tsx
+++ src/components/Transition/transition.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, { ReactNode } from 'react'
 import { CSSTransition } from 'react-transition-group'
-import type { CSSTransitionProps } from 'react-transition-group/CSSTransition'
+import { CSSTransitionProps } from 'react-transition-group/CSSTransition'
 
 type AnimationName =
   | 'zoom-in-top'
@@ -10,22 +10,24 @@ type AnimationName =
 
 type TransitionProps = CSSTransitionProps & {
   animation?: AnimationName
+  wrapper?: boolean
+  children?: ReactNode
 }
 
-export const Transition: React.FC<TransitionProps> = (props) => {
-  const { children, classNames, animation, ...restProps } = props
-
+const Transition: React.FC<TransitionProps> = (props) => {
+  const { children, classNames, animation, wrapper, ...restProps } = props
   return (
     <CSSTransition
       classNames={classNames ? classNames : animation}
       {...restProps}
     >
-      {children}
+      {wrapper ? <div>{children}</div> : children}
     </CSSTransition>
   )
 }
-
 Transition.defaultProps = {
   unmountOnExit: true,
   appear: true,
 }
+
+export default Transition
