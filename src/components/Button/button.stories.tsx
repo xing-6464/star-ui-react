fdbd8e58594de186663ce7823175f70ee52f03f0
xing-6464 <xing_6464@foxmail.com>
2023-03-19 14:38:53 +0800

    storybook 中的 args 的概念

--- src/components/Button/button.stories.tsx
+++ src/components/Button/button.stories.tsx
@@ -7,32 +7,61 @@ const buttonMeta: ComponentMeta<typeof Button> = {
   title: 'Button',
   component: Button,
 }
-
 export default buttonMeta
 
-export const Default: ComponentStory<typeof Button> = () => (
-  <Button>Default Button</Button>
-)
+const Template: ComponentStory<typeof Button> = (args) => {
+  return <Button {...args}></Button>
+}
 
-Default.storyName = '默认按钮样式'
+export const Default = Template.bind({})
+Default.args = {
+  children: 'Default Button',
+}
 
-export const ButtonWithSize: ComponentStory<typeof Button> = () => (
-  <>
-    <Button size="lg">large button</Button>
-    <Button size="sm">small button</Button>
-  </>
-)
+export const Large = Template.bind({})
+Large.args = {
+  size: 'lg',
+  children: 'Large Button',
+}
+export const Small = Template.bind({})
+Small.args = {
+  size: 'sm',
+  children: 'Small Button',
+}
+export const Primary = Template.bind({})
+Primary.args = {
+  btnType: 'primary',
+  children: 'Primary Button',
+}
+export const Danger = Template.bind({})
+Danger.args = {
+  btnType: 'danger',
+  children: 'Danger Button',
+}
+export const Link = Template.bind({})
+Link.args = {
+  btnType: 'link',
+  children: 'Link Button',
+  href: 'https://google.com',
+}
 
-ButtonWithSize.storyName = '不同尺寸按钮'
+// export const ButtonWithSize: ComponentStory<typeof Button> = () => (
+//   <>
+//     <Button size="lg">large button</Button>
+//     <Button size="sm">small button</Button>
+//   </>
+// )
 
-export const ButtonWithType: ComponentStory<typeof Button> = () => (
-  <>
-    <Button btnType="primary">默认</Button>
-    <Button btnType="danger">警告</Button>
-    <Button btnType="link" href="https://google.com">
-      链接
-    </Button>
-  </>
-)
+// ButtonWithSize.storyName = '不同尺寸按钮'
 
-ButtonWithType.storyName = '不同样式'
+// export const ButtonWithType: ComponentStory<typeof Button> = () => (
+//   <>
+//     <Button btnType="primary">默认</Button>
+//     <Button btnType="danger">警告</Button>
+//     <Button btnType="link" href="https://google.com">
+//       链接
+//     </Button>
+//   </>
+// )
+
+// ButtonWithType.storyName = '不同样式'
