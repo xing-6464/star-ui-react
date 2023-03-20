34375ec33c219593f9b81c838391acfc140dd5f8
xing-6464 <xing_6464@foxmail.com>
2023-03-20 17:09:28 +0800

    Input组件

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './styles/index.scss'
 
-import App from './App'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fas } from '@fortawesome/free-solid-svg-icons'
-
-library.add(fas)
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <React.StrictMode></React.StrictMode>
 )
