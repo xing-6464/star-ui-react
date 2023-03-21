81c39e49c82d8aaf7305cc82a1e8418ba58ebda5
xing-6464 <xing_6464@foxmail.com>
2023-03-22 00:23:43 +0800

    使用 renderProps 自定义展示

--- src/components/Form/form.stories.tsx
+++ src/components/Form/form.stories.tsx
@@ -20,6 +20,15 @@ const confirmRules: CustomRule[] = [
     asyncValidator(rule, value) {
       console.info('the value', getFiledValue['password'])
       console.info(value)
+
+      return new Promise((resolve, reject) => {
+        if (value !== getFiledValue(['password'])) {
+          reject('The two password that you entered do not match')
+        }
+        setTimeout(() => {
+          resolve()
+        }, 1000)
+      })
       if (value !== getFiledValue(['password'])) {
         return Promise.reject('The two password that you entered do not match')
       }
@@ -30,44 +39,49 @@ const confirmRules: CustomRule[] = [
 export const BasicForm = (args: any) => {
   return (
     <Form initialValues={{ username: 'xing', agreement: true }} {...args}>
-      <Item
-        label="用户名"
-        name="username"
-        rules={[{ type: 'email', required: true }]}
-      >
-        <Input />
-      </Item>
-      <Item
-        label="密码"
-        name="password"
-        rules={[{ type: 'string', required: true, min: 3, max: 8 }]}
-      >
-        <Input type="password" />
-      </Item>
-      <Item label="重复密码" name="confirmPwd" rules={confirmRules}>
-        <Input type="password" />
-      </Item>
-      <div
-        className="agreement-section"
-        style={{ display: 'flex', justifyContent: 'center' }}
-      >
-        <Item
-          name="agreement"
-          valuePropName="checked"
-          getValueFromEvent={(e) => e.target.checked}
-          rules={[{ type: 'enum', enum: [true], message: '请同意协议' }]}
-        >
-          <input type="checkbox" />
-        </Item>
-        <span className="agree-text">
-          注册即代表你同意<a href="#">用户协议</a>
-        </span>
-      </div>
-      <div className="star-form-submit-area">
-        <Button type="submit" btnType="primary">
-          登录
-        </Button>
-      </div>
+      {({ isValid, isSubmitting }) => (
+        <>
+          <Item
+            label="用户名"
+            name="username"
+            rules={[{ type: 'email', required: true }]}
+          >
+            <Input />
+          </Item>
+          <Item
+            label="密码"
+            name="password"
+            rules={[{ type: 'string', required: true, min: 3, max: 8 }]}
+          >
+            <Input type="password" />
+          </Item>
+          <Item label="重复密码" name="confirmPwd" rules={confirmRules}>
+            <Input type="password" />
+          </Item>
+          <div
+            className="agreement-section"
+            style={{ display: 'flex', justifyContent: 'center' }}
+          >
+            <Item
+              name="agreement"
+              valuePropName="checked"
+              getValueFromEvent={(e) => e.target.checked}
+              rules={[{ type: 'enum', enum: [true], message: '请同意协议' }]}
+            >
+              <input type="checkbox" />
+            </Item>
+            <span className="agree-text">
+              注册即代表你同意<a href="#">用户协议</a>
+            </span>
+          </div>
+          <div className="star-form-submit-area">
+            <Button type="submit" btnType="primary">
+              登录 {isSubmitting ? '验证中' : '验证完毕'}{' '}
+              {isValid ? '通过' : '没有通过'}
+            </Button>
+          </div>
+        </>
+      )}
     </Form>
   )
 }
--- src/components/Form/form.tsx
+++ src/components/Form/form.tsx
@@ -2,13 +2,16 @@ import React, { createContext, ReactNode } from 'react'
 import useStore from './useStore'
 
 import type { ValidateError } from 'async-validator'
+import { FormState } from './useStore'
+
+export type RenderProps = (form: FormState) => ReactNode
 
 export interface FormProps {
   /**表单名称，会作为表单字段 id 前缀使用 */
   name?: string
   /**表单默认值，只有初始化以及重置时生效 */
   initialValues?: Record<string, any>
-  children?: ReactNode
+  children?: ReactNode | RenderProps
   /**提交表单且数据验证成功后回调事件 */
   onFinish?: (values: Record<string, any>) => void
   /**提交表单且数据验证失败后回调事件 */
@@ -47,11 +50,17 @@ export const Form: React.FC<FormProps> = (props) => {
       onFinishFailed(values, errors)
     }
   }
+  let childrenNode: ReactNode
+  if (typeof children === 'function') {
+    childrenNode = children(form)
+  } else {
+    childrenNode = children
+  }
   return (
     <>
       <form name={name} className="star-form" onSubmit={submitForm}>
         <FormContext.Provider value={passedContext}>
-          {children}
+          {childrenNode}
         </FormContext.Provider>
       </form>
       <div>
