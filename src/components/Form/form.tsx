be80b2906073333e52c97b2462586d83c61fec0f
xing-6464 <xing_6464@foxmail.com>
2023-03-21 21:17:28 +0800

    初始化 useStore

--- src/components/Form/form.tsx
+++ src/components/Form/form.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import useStore from './useStore'
 
 export interface FormProps {
   name?: string
@@ -7,11 +8,18 @@ export interface FormProps {
 
 export const Form: React.FC<FormProps> = (props) => {
   const { name, children } = props
+  const { form, fields } = useStore()
 
   return (
-    <form name={name} className="star-form">
-      {children}
-    </form>
+    <>
+      <form name={name} className="star-form">
+        {children}
+      </form>
+      <div>
+        <pre style={{ whiteSpace: 'pre-wrap' }}>{JSON.stringify(fields)}</pre>
+        <pre style={{ whiteSpace: 'pre-wrap' }}>{JSON.stringify(form)}</pre>
+      </div>
+    </>
   )
 }
 
