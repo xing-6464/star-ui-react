795b5097ef1258ea101fde0c7b66b4ebb831f527
xing-6464 <xing_6464@foxmail.com>
2023-03-22 02:13:38 +0800

    驯服 tsc - tsconfig 编写第二部分

--- src/components/Tabs/tabs.tsx
+++ src/components/Tabs/tabs.tsx
@@ -1,6 +1,6 @@
 import React, { FC, useState, FunctionComponentElement, ReactNode } from 'react'
 import classNames from 'classnames'
-import { TabItemProps } from '.'
+import { TabItemProps } from './tabItem'
 
 export interface TabsProps {
   /**当前激活 tab 面板的 index，默认为0 */
